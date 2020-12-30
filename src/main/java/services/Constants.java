e05d59191f332727757ae42d9a3c0e2cbcf8d83a
DiabateJS <diabatejs@yahoo.fr>
2020-12-30 07:48:47 +0100

    Config de la base de production.

--- src/main/java/services/Constants.java
+++ src/main/java/services/Constants.java
@@ -2,28 +2,28 @@ package services;
 
 public class Constants {
     public static String DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static String URL_DATABASE = "jdbc:mysql://localhost/project_manager?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
-    public static String USERNAME = "root";
-    public static String PASSWORD = "";
+    public static String URL_DATABASE = "jdbc:mysql://185.98.131.90/project_manager?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+    public static String USERNAME = "djste1070339";
+    public static String PASSWORD = "da6ysjpqpp";
 
     //SQL GESTION PROJET
-    public static String SELECT_PROJECTS_QUERY = "select * from projet";
-    public static String SELECT_PROJECTS_TASK_QUERY = "select id, libelle, estimation, description, etat, (select fullname from users where  id = tache.idUser ) as user from tache where idprojet = ?";
-    public static String INSERT_PROJECT_QUERY = "insert into projet(libelle, etat, description) values (?, ?, ?)";
-    public static String DELETE_PROJECT_TASK_QUERY = "delete from tache where idProjet = ?";
-    public static String DELETE_PROJECT_QUERY = "delete from projet where id = ?";
-    public static String UPDATE_PROJECT_QUERY = "update projet set libelle = ? , etat = ? , description = ? where id = ?";
+    public static String SELECT_PROJECTS_QUERY = "select * from pm_projet";
+    public static String SELECT_PROJECTS_TASK_QUERY = "select id, libelle, estimation, description, etat, (select fullname from pm_users where  id = tache.idUser ) as user from pm_tache where idprojet = ?";
+    public static String INSERT_PROJECT_QUERY = "insert into pm_projet(libelle, etat, description) values (?, ?, ?)";
+    public static String DELETE_PROJECT_TASK_QUERY = "delete from pm_tache where idProjet = ?";
+    public static String DELETE_PROJECT_QUERY = "delete from pm_projet where id = ?";
+    public static String UPDATE_PROJECT_QUERY = "update pm_projet set libelle = ? , etat = ? , description = ? where id = ?";
 
     //SQL GESTION TACHE
-    public static String INSERT_PROJECT_TASK_QUERY = "insert into tache (libelle, estimation, description, etat, idProjet, idUser) values (?, ?, ?, ?, ?, (select id from users where fullname = ?))";
-    public static String DELETE_TASK_QUERY = "delete from tache where idProjet = ? and id = ?";
-    public static String UPDATE_TASK_QUERY = "update tache set  libelle = ? ,estimation = ?, etat = ? , description = ? , idUser = (select id from users where fullname = ? ) where id = ? and idProjet = ?";
+    public static String INSERT_PROJECT_TASK_QUERY = "insert into pm_tache (libelle, estimation, description, etat, idProjet, idUser) values (?, ?, ?, ?, ?, (select id from users where fullname = ?))";
+    public static String DELETE_TASK_QUERY = "delete from pm_tache where idProjet = ? and id = ?";
+    public static String UPDATE_TASK_QUERY = "update pm_tache set  libelle = ? ,estimation = ?, etat = ? , description = ? , idUser = (select id from users where fullname = ? ) where id = ? and idProjet = ?";
 
-    public static String AUTH_USERS_QUERY = "select * from users where login = ? and password = ?";
-    public static String SELECT_USERS_QUERY = "select * from users";
-    public static String INSERT_USER_QUERY = "insert into users(fullname, login, password, email, profile) values (?, ?, ?, ?, ?)";
-    public static String DELETE_USER_QUERY = "delete from users where id = ?";
-    public static String UPDATE_USER_QUERY = "update users set  fullname = ? ,login = ?, password = ? , email = ?, profile = ?  where id = ? ";
+    public static String AUTH_USERS_QUERY = "select * from pm_users where login = ? and password = ?";
+    public static String SELECT_USERS_QUERY = "select * from pm_users";
+    public static String INSERT_USER_QUERY = "insert into pm_users (fullname, login, password, email, profile) values (?, ?, ?, ?, ?)";
+    public static String DELETE_USER_QUERY = "delete from pm_users where id = ?";
+    public static String UPDATE_USER_QUERY = "update pm_users set  fullname = ? ,login = ?, password = ? , email = ?, profile = ?  where id = ? ";
 
     public static String SUCCES_CODE = "SUCCES";
     public static String ERROR_CODE = "ERROR";
